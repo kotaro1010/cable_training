ad0f3b34360a916b3a263b021933d6b8ec87d35d
k.hara <k.hara@effect-effect.com>
2023-07-19 11:03:06 +0900

    見逃したサンプルを取得できるよう追加

--- predict.py
+++ predict.py
@@ -6,6 +6,7 @@ import pandas as pd
 import torch
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, precision_score, recall_score
 from torch.utils.data import DataLoader
+import torchvision
 
 from image_datatset import CableCrackImageDataset
 from model import CrackDetectionModel
@@ -33,15 +34,16 @@ def main():
     # TODO 本番では解除するように。
     torch.cuda.set_per_process_memory_fraction(0.2, 0)
 
-    # img_dir_for_test = "./dataset/dev3_testing_correction_splited/test"
+    img_dir_for_test = "./dataset/dev3_testing_correction_v2_splited/test"
     # img_dir_for_test = "./dataset/Correction"
-    img_dir_for_test = "dataset/splited_normal_anomalous_v5/test"
+    # img_dir_for_test = "dataset/splited_normal_anomalous_v5/test"
     dataset_test = CableCrackImageDataset(images_directory=img_dir_for_test)
     dataloader_test = DataLoader(dataset=dataset_test, batch_size=8, shuffle=False)
     # model = CrackDetectionModel.load_from_checkpoint("lightning_logs/version_4/checkpoints/epoch=9-step=710.ckpt")
-    model = CrackDetectionModel.load_from_checkpoint(
-        "saved_models/model_ckpt/2023-06-09T20:17:22.500886+09:00_iconic-monkey-13_epoch=0_step=411_val_loss=0.17.ckpt"
-    )
+    model_path = "saved_models/model_ckpt/2023-07-05T22:21:47.341853+09:00_dainty-universe-95_epoch=8_step=2520_val_loss=0.14.ckpt"
+    model_name = os.path.basename(model_path)
+    wandb_run_name = model_name.split("_")[1]
+    model = CrackDetectionModel.load_from_checkpoint(model_path)
     trainer = pl.Trainer(
         accelerator="gpu",
         devices=1,
@@ -56,6 +58,23 @@ def main():
         print(precision_score(dataset_test.labels, predictions))
         print(classification_report(dataset_test.labels, predictions))
         print(confusion_matrix(dataset_test.labels, predictions))
+
+        # 見逃したサンプルの保存
+        true_pos = torch.where(torch.tensor(dataset_test.labels) == 1)
+        pred_neg = torch.where(predictions == 0)
+        miss_sample_idx = set(true_pos[0].numpy()) & set(pred_neg[0].numpy())
+        for idx in miss_sample_idx:
+            img_path = dataset_test.all_imgs[idx]
+            img_name = os.path.basename(img_path)
+            img_type = img_name.split(".")[-1]
+            img = torchvision.io.read_image(img_path)
+            os.makedirs(f"./miss_sample/{wandb_run_name}", exist_ok=True)
+            if img_type == "png":
+                torchvision.io.write_png(img, f"./miss_sample/{wandb_run_name}/{img_name}")
+            elif img_type == "jpg" or img_type == "jpeg":
+                torchvision.io.write_jpeg(img, f"./miss_sample/{wandb_run_name}/{img_name}")
+            else:
+                assert False
 
     #########
     # 後処理 #
